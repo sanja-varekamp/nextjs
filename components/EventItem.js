23d1c1e90730ffb547fe6c903889468343afbd30
Sanja <sanjafennec@gmail.com>
2022-11-02 16:05:48 +0100

    add events componenet

---
+++ components/EventItem.js
@@ -0,0 +1,7 @@
+import Link from 'next/link';
+import Image from 'next/link';
+import styles from '@/styles/EventItem.module.css';
+
+export default function EventItem() {
+	return <div>EventItem</div>;
+}
