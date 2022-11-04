9cb40313e460c707e483a4e51c819d377373d15c
Sanja <sanjafennec@gmail.com>
2022-11-04 13:07:03 +0100

    Add events and a single event page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,87 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
+import styles from '@/styles/Events.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaPencilAlt, FaTimes } from 'react-icons/fa';
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
+	const deleteEvent = (e) => {
+		console.log('delete');
+	};
 	return (
 		<Layout>
-			<h1>My Event</h1>
+			<div className={styles.event}>
+				<div className={styles.control}>
+					<Link href={`/events/edit/${evt.id}`}>
+						<a>
+							<FaPencilAlt /> Edit Event
+						</a>
+					</Link>
+					<a href="#" className={styles.delete} onClick={deleteEvent}>
+						<FaTimes /> Delete Event
+					</a>
+				</div>
+
+				<span>
+					{evt.date} at {evt.time}
+					<h1>{evt.name}</h1>
+					{evt.image && (
+						<div className={styles.image}>
+							<Image src={evt.image} width={960} height={600} />
+						</div>
+					)}
+					<h3> Performers:</h3>
+					<p>{evt.performers}</p>
+					<h3> Description:</h3>
+					<p>{evt.description}</p>
+					<h3> Venue: {evt.venue}</h3>
+					<p>{evt.address}</p>
+					<Link href="/events">
+						<a className={styles.back}>{'<'} Go back</a>
+					</Link>
+				</span>
+			</div>
 		</Layout>
 	);
 }
+
+// Data Fetching using getStaticProps() (with getStaticPaths())
+
+export async function getStaticPaths() {
+	const res = await fetch(`${API_URL}/api/events`);
+	const events = await res.json();
+
+	const paths = events.map((evt) => ({
+		params: { slug: evt.slug },
+	}));
+
+	return {
+		paths,
+		fallback: true,
+	};
+}
+
+export async function getStaticProps({ params: { slug } }) {
+	const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+	const events = await res.json();
+	return {
+		props: {
+			evt: events[0],
+			revalidate: 1,
+		},
+	};
+}
+
+// Data Fetching using getServerSideProps()
+// export async function getServerSideProps({ query: { slug } }) {
+// 	const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+// 	const events = await res.json();
+// 	return {
+// 		props: {
+// 			evt: events[0],
+// 		},
+// 	};
+// }
